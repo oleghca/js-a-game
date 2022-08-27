7826fc0092a4a745d211c8dc8a7dd8c71fa6028e
oleghca <108630955+oleghca@users.noreply.github.com>
2022-08-27 19:01:13 +0300

    upload site

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+
+
+
+let probe = 10;
+
+ 
+    let n = Math.random();
+    n = Math.floor(n * 100);
+    n = n + 1;
+    
+
+
+console.log(n)
+
+
+
+
+function play(){
+    let num = +numOption.value;
+     if(num > n){
+    
+        balBet.innerHTML = 'Больше моего';
+        probe--;
+        tabl.innerHTML = probe;
+     }
+        if(num < n){
+            balBet.innerHTML = 'Меньше моего';
+            probe--;
+            tabl.innerHTML = probe;
+        }
+        if(probe == 0){
+            balBet.innerHTML = 'Вы проиграли';
+            
+            
+        }
+
+        
+         if(num == n){
+        balBet.innerHTML = 'Вы выйграли';
+        return;
+        
+    }
+    
+    
+    
+}
+
+    
